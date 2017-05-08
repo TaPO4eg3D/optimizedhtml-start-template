32bcbcbdc29b62b892ecbef289041f0bd1d18ee7
TaPO4eg3D <fo.denis2011@gmail.com>
2017-05-08 23:32:53 +0700

    Added Bootstrap dependencies, the latest version of font-awesome, and cleaned up a bit some files.

--- gulpfile.js
+++ gulpfile.js
@@ -27,6 +27,8 @@ gulp.task('common-js', function() {
 gulp.task('js', ['common-js'], function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/tether/dist/tether.min.js',
+		'app/libs/bootstrap/js/bootstrap.min.js',
 		'app/js/common.min.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
@@ -65,7 +67,7 @@ gulp.task('watch', ['sass', 'js', 'browser-sync'], function() {
 gulp.task('imagemin', function() {
 	return gulp.src('app/img/**/*')
 	.pipe(cache(imagemin()))
-	.pipe(gulp.dest('dist/img')); 
+	.pipe(gulp.dest('dist/img'));
 });
 
 gulp.task('build', ['removedist', 'imagemin', 'sass', 'js'], function() {
